c73a6c06e0a3e28eea84cfa76a25577c05f31ac5
angieM-30 <madronaangelica30@gmail.com>
2023-10-01 12:48:31 +0800

    modified

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
+<script setup>
+import { ref } from 'vue'
 
+const input = ref('web')
+</script>
+<template>
+    <!-- reactivity-->
+    <!--two-way binding-->
+    <select v-model="input">
+    <option value="web">web</option>
+    <option value="net">net</option>
+    <option value="others">others</option>
+    </select>
+    <p>{{ input }}</p>
+    <!--conditional-->
+    <p v-if="input =='web'"> Web Development</p>
+    <p v-else-if="input == 'net'">Networking</p>
+    <p v-else>others</p>
 
-<template></template>
+    <p v-if="input == 'web'"> Web Development</p> 
+    <p v-else>others</p>
+    <!--commit and push-->
+
+    </template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/js/index.umd.js'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/js/index.umd.js";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
